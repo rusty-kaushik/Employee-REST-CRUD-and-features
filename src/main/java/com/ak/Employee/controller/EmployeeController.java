5b27cb6273886abe1116efefc90238bbe4b6b647
Aniket Kaushik <aniket.kaushik@in2ittech.com>
2024-05-17 10:07:05 +0530

    added swagger dependency

--- src/main/java/com/ak/Employee/controller/EmployeeController.java
+++ src/main/java/com/ak/Employee/controller/EmployeeController.java
@@ -3,11 +3,18 @@ package com.ak.Employee.controller;
 import com.ak.Employee.dto.EmployeeDto;
 import com.ak.Employee.response.ResponseHandler;
 import com.ak.Employee.service.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Employee", description = "Employee management APIs")
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
@@ -16,6 +23,14 @@ public class EmployeeController {
     EmployeeService empService;
 
     // CREATE A EMPLOYEE
+    @Operation(
+            summary = "Create a Employee",
+            description = "Creates a Employee. The API takes id, name, age, salary and joining date in json.",
+            tags = { "employee", "post" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", content = { @Content(schema = @Schema(implementation = String.class), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "422", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) }) })
     @PostMapping
     public ResponseEntity<Object> createEmployee(@RequestBody EmployeeDto empDto){
         String result = empService.createEmployee(empDto);
@@ -23,6 +38,14 @@ public class EmployeeController {
     }
 
     // DELETE A EMPLOYEE
+    @Operation(
+            summary = "Delete a Employee by Id",
+            description = "Deletes a Employee  by specifying its id.",
+            tags = { "employee", "delete" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = String.class), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) }) })
     @DeleteMapping("/{employeeId}")
     public  String deleteEmployee(@PathVariable("employeeId") String employeeId)
     {
@@ -30,6 +53,14 @@ public class EmployeeController {
     }
 
     // GET EMPLOYEE BY ID
+    @Operation(
+            summary = "Retrieve a Employee by Id",
+            description = "Get a Employee  by specifying its id. The response is Employee object with id, name, age and joining date.",
+            tags = { "employee", "get" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = EmployeeDto.class), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) }) })
     @GetMapping("/{employeeId}")
     public ResponseEntity<Object> getEmployee(@PathVariable("employeeId") String employeeId)
     {
@@ -38,6 +69,14 @@ public class EmployeeController {
     }
 
     // UPDATE EMPLOYEE BY ID
+    @Operation(
+            summary = "Update a Employee by Id",
+            description = "Updates a Employee by specifying its id. The response is a success message.",
+            tags = { "employee", "put" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = String.class), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) }) })
     @PutMapping("/{employeeId}")
     public ResponseEntity<Object> updateEmployee(@PathVariable("employeeId") String employeeId, @RequestBody EmployeeDto empDto) {
             return ResponseHandler.responseBuilder("Employee Updated Successfully", HttpStatus.OK,empService.updateEmployee(employeeId, empDto));
