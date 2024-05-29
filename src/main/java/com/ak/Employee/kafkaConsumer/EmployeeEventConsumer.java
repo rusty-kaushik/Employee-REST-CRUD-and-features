f238260cd39576376b49a3b2cf2bc4e2e26f8ef2
Aniket Kaushik <aniket.kaushik@in2ittech.com>
2024-05-29 12:18:04 +0530

    test commit

--- src/main/java/com/ak/Employee/kafkaConsumer/EmployeeEventConsumer.java
+++ src/main/java/com/ak/Employee/kafkaConsumer/EmployeeEventConsumer.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class EmployeeEventConsumer {
-Hi
+
     @KafkaListener(topics = "employee-events", groupId = "employee_group")
     public void listen(String message) {
         System.out.println("Received message: " + message);
