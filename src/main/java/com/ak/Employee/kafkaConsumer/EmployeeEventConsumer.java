9dd2f5ffef3f92d5c86e7afe5130b8aa541010df
Aniket Kaushik <aniket.kaushik@in2ittech.com>
2024-05-29 12:08:10 +0530

    unique EMP ID

--- src/main/java/com/ak/Employee/kafkaConsumer/EmployeeEventConsumer.java
+++ src/main/java/com/ak/Employee/kafkaConsumer/EmployeeEventConsumer.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class EmployeeEventConsumer {
 
-    @KafkaListener(topics = "employee_events", groupId = "employee_group")
+    @KafkaListener(topics = "employee-events", groupId = "employee_group")
     public void listen(String message) {
         System.out.println("Received message: " + message);
     }
